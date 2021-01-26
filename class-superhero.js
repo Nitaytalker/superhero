0589feb6c4fe7b9ee9c63e5ca485148b482c73a4
Nitaytalker <nitay68428@gmail.com>
2021-01-26 13:53:26 +0200

    add class superhero

---
+++ class-superhero.js
@@ -0,0 +1,12 @@
+class Superhero{
+    constructor(name, mainStrength, picture){
+        this.name=name;
+        this.mainStrength=mainStrength;
+        this.picture=picture;
+    }
+}
+
+const allHeros = [
+    new Superhero("Supermen","Fly",'Superman_Vol_5_1_Textless.jpg'),
+    new Superhero("Spidermen","Spider",'spider-man.png')
+];
