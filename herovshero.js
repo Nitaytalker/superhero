3ad62ec1e28dc046563213cbaf4c0c419119083d
Nitaytalker <nitay68428@gmail.com>
2021-01-28 16:25:17 +0200

    asas

--- herovshero.js
+++ herovshero.js
@@ -1,4 +1,5 @@
 function randomFight() {
+    console.log(allHeros.length);
     if(allHeros.length<=1){
         window.location.href = 'add-hero.html';
         alert('need 2 hero min');
@@ -6,7 +7,7 @@ function randomFight() {
     }
     let toFight1 = Math.floor(Math.random() * allHeros.length);
     let toFight2 = Math.floor(Math.random() * allHeros.length);
-    if()
+    
     while (toFight1 == toFight2) {
         toFight2 = Math.floor(Math.random() * allHeros.length);
     }
@@ -15,19 +16,27 @@ function randomFight() {
      }, 5000);
    
     document.getElementById("card1").innerHTML = `
-    <img src="${allHeros[toFight1].picture}"
-     class="card-img-top" alt="..."></img>
-     <div class="card-body">
-     <h5 class="card-title">Name:${allHeros[toFight1].name}</h5>
-     <p class="card-text">Power: ${allHeros[toFight1].mainStrength}</p>
-    </div>`;
+    <div class="">
+                <div class="responsive">
+                    <div class="gallery">
+                        <a target="_blank">
+                            <img src="${allHeros[toFight1].picture}" alt="Cinque Terre" width="400" height="200">
+                        </a>
+                        <div class="desc">Name:${allHeros[toFight1].name}.<br> Power: ${allHeros[toFight1].mainStrength}</div>
+                    </div>
+                </div>
+            </div>`;
     document.getElementById("card2").innerHTML = `
-    <img src="${allHeros[toFight2].picture}"
-     class="card-img-top" alt="..."></img>
-     <div class="card-body">
-     <h5 class="card-title">Name:${allHeros[toFight2].name}</h5>
-     <p class="card-text">Power: ${allHeros[toFight2].mainStrength}</p>
-    </div>`;
+    <div class="">
+                <div class="responsive">
+                    <div class="gallery">
+                        <a target="_blank">
+                            <img src="${allHeros[toFight2].picture}" alt="Cinque Terre" width="400" height="200">
+                        </a>
+                        <div class="desc">Name:${allHeros[toFight2].name}.<br> Power: ${allHeros[toFight2].mainStrength}</div>
+                    </div>
+                </div>
+            </div>`;
 
     document.getElementById("removehidden").classList.remove("hidden");
 }
