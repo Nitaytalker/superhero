51d85a794d160637bfb83d65004b0cecbde3c092
ohadhayun <ohadhayun4@Walla.com>
2021-01-28 15:21:16 +0200

    adding page 3

--- class-superhero.js
+++ class-superhero.js
@@ -5,11 +5,7 @@ class Superhero{
         this.picture=picture;
     }
 }
-const allHeros = [
-    new Superhero("Supermen","Fly",'Superman_Vol_5_1_Textless.jpg'),
-    new Superhero("Spidermen","Spider",'spider-man.png'),
-    new Superhero("Thor", "Hummer", "thor.jpg")
-];
+const allHeros = [];
 
 function inStart(){
     let heros = window.localStorage.getItem("localHero");
--- page3.js
+++ page3.js
@@ -5,14 +5,14 @@ for(const hero  of allHeros){
     // console.log(hero);
     // console.log(hero.name, hero.mainStrength, hero.picture);
 }
-
+console.log(allHeros);
 let counter = -1;
 const globalma = [];
 function changeIamgeOfHero(hero){
     for(const heroCheck of allHeros){
         if(heroCheck.name == hero.id){
             document.querySelector('#print').innerHTML =`<img src="${heroCheck.picture}" alt="Cinque Terre" width="300" height="200">`;
-            console.log(heroCheck.picture);
+            // console.log(heroCheck.picture);
             globalma.push(heroCheck);
             counter++;
         }
@@ -25,7 +25,21 @@ function changeHeroPic(){
    globalma[counter].picture = newSrc;
    document.querySelector('#print').innerHTML =`<img src="${newSrc}" alt="Cinque Terre" width="300" height="200">`;
    alert('The image has been updated');
-   localStorage.setItem("newSrc", );
+    // console.log(globalma[counter]);
+    let allHeros = window.localStorage.getItem("localHero");
+    if(allHeros==null){
+        allHeros =[];
+       
+    }else{
+        allHeros= JSON.parse(localStorage.getItem("localHero")) 
+    }
+
+    for(const superHero of allHeros){
+        if(superHero.name == globalma[counter].name){
+            superHero.picture = globalma[counter].picture;
+        }
+    }
+    window.localStorage.setItem("localHero",JSON.stringify(allHeros));
 }
 
 
